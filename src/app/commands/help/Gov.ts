d22f6f6f26601a63a801c93ff4a61f52ff7d5272
Joelone <1656353+Joelone@users.noreply.github.com>
2022-02-28 22:37:23 -0800

    Add gov info
    Signed-off-by: Joelone <1656353+Joelone@users.noreply.github.com>

--- src/app/commands/help/Gov.ts
+++ src/app/commands/help/Gov.ts
@@ -4,6 +4,7 @@ import {
 	SlashCommand,
 	SlashCreator,
 } from 'slash-create';
+import GovInfo from '../../service/help/GovInfo';
 import HelpMe from '../../service/help/HelpMe';
 import { LogUtils } from '../../utils/Log';
 
@@ -34,7 +35,7 @@ export default class Help extends SlashCommand {
 		let messageOptions: MessageOptions;
 		switch (ctx.subcommands[0]) {
 		case 'info':
-			messageOptions = HelpMe();
+			messageOptions = GovInfo();
 			break;
 		}
 		return ctx.send(messageOptions);
