5b0d582f5ac6aee8cb1d192c6e61c03f61f56d6e
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-12-27 12:40:56 -0700

    lint

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,8 +1,8 @@
 const ApiUtils = {
 
-    getBasePath(){
-        return `${process.env.GOVERNATOR_API_BASE_PATH}/${process.env.GOVERNATOR_API_PREFIX}`
-    }
-}
+	getBasePath() {
+		return `${process.env.GOVERNATOR_API_BASE_PATH}/${process.env.GOVERNATOR_API_PREFIX}`;
+	},
+};
 
 export default ApiUtils;
