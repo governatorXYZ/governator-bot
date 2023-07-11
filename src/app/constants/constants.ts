66edbc76a3292b5ee6c5609caf5025a909875d2f
Zach Berwaldt <zberwaldt@tutamail.com>
2023-07-10 20:30:55 -0400

    add fallbacks

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
 export default Object.freeze({
     APP_VERSION: process.env.npm_package_version,
     APP_NAME: 'Governator',
-    SSE_URL: `${process.env.GOVERNATOR_API_BASE_PATH}/${process.env.GOVERNATOR_API_PREFIX}/${process.env.GOVERNATOR_API_ENDPOINT_SSE}`,
+    SSE_URL: `${process.env.GOVERNATOR_API_BASE_PATH}/${process.env.GOVERNATOR_API_PREFIX}/${process.env.GOVERNATOR_API_ENDPOINT_SSE ?? 'sse'}`,
 });
 
 export const strategyTypes = Object.freeze({
